7d645049355746d9cbb62f3b5cb318106a005205
Oğuzhan <ouzhan.ysl@gmail.com>
2021-11-20 13:27:25 +0300

    nft

--- case1/src/pages/Header.js
+++ case1/src/pages/Header.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const HeaderWrapper = styled.div`
   display: grid;
+  grid-template-rows: auto auto;
   background-color: green;
   justify-content: center;
 `;
 
 const Header = () => {
+  const [isConnected, setIsConnected] = useState(false);
+
+  const connectWallet = async () => {
+    try {
+      const resp = await window.solana.connect();
+      console.log("Responsive: ", resp)
+      resp.publicKey.toString()
+
+      // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo
+      // My Key: 29QPT5gnSUWtxnH54L5vfZMNmpqBVdDFcWWE5ndwHYgjuKwDViFhBDSYuxUZmMUj8gZaG6gcDDs73fPMpdTjCYia
+      setIsConnected(true);
+    } catch (err) {
+      console.log(err)
+      // { code: 4001, message: 'User rejected the request.' }
+    }
+  }
+  const disconnectWallet = () => {
+    window.solana.disconnect();
+    window.solana.on('disconnect', () => console.log("disconnected!"))
+    setIsConnected(false);
+  }
+
   return (
     <HeaderWrapper>
       <h1>Oğuzhan Uysal</h1>
+      {
+        isConnected
+          ?
+          <>
+            <button onClick={() => disconnectWallet()}>Bağlantıyı Kes</button>
+          </>
+          :
+          <button onClick={() => connectWallet()}>Bağla</button>
+      }
     </HeaderWrapper>
   )
 }
--- case1/src/pages/Home.js
+++ case1/src/pages/Home.js
@@ -1,18 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from './Header';
 
+// Create connection
+import { Connection, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { getParsedNftAccountsByOwner, isValidSolanaAddress, createConnectionConfig, } from "@nfteyez/sol-rayz";
+import axios from 'axios';
+
+
 const HomeWrapper = styled.div`
   display: grid;
-  background-color: green;
+  grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+  background-color: gray;
+  margin: 1rem 0;
+  .card-container {
+    display: grid;
+    grid-template-rows: auto auto auto auto;
+    justify-items: center;
+  }
 `;
 
 const Home = () => {
+  const [isConnected, setisConnected] = useState(false);
+  const [nftData, setNftData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //create a connection of devnet
+  const createConnection = () => {
+    return new Connection(clusterApiUrl("devnet"));
+  };
+  createConnection();
+
+  //check solana on window. This is useful to fetch address of your wallet.
+  const getProvider = () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        return provider;
+      }
+    }
+  };
+
+  //Function to get all NFT information.
+  //get NFT
+  const getAllNftData = async () => {
+    try {
+      const connect = createConnectionConfig(clusterApiUrl("devnet"));
+      const provider = getProvider();
+      let ownerToken = provider.publicKey;
+      const result = isValidSolanaAddress(ownerToken);
+      console.log("result", result);
+      const nfts = await getParsedNftAccountsByOwner({
+        publicAddress: ownerToken,
+        connection: connect,
+        serialization: true,
+      });
+      return nfts;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Function to get all nft data
+  const getNftTokenData = async () => {
+    try {
+      let nftData = await getAllNftData();
+      var data = Object.keys(nftData).map((key) => nftData[key]); let arr = [];
+      let n = data.length;
+      for (let i = 0; i < n; i++) {
+        console.log(data[i].data.uri);
+        let val = await axios.get(data[i].data.uri);
+        arr.push(val);
+      }
+      return arr;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function data() {
+      let res = await getAllNftData();
+      setNftData(res);
+      setIsLoading(true);
+    }
+    console.log(nftData)
+    data();
+  }, [isConnected]);
+
+
   return (
     <>
       <Header />
       <HomeWrapper>
-        Home Page
+        {
+          nftData &&
+          nftData.length > 0 &&
+          nftData.map((val, ind) => {
+            return (
+              <div className="card-container" onClick={null} key={ind}>
+                <img src={val.data.image} alt="loading..." />
+                <div className="">{val.data.name}</div>
+                <div className="">{val.data.sellerFeeBasisPoints}</div>
+                <div className="">{val.mint}</div>
+              </div>
+            );
+          })
+        }
       </HomeWrapper>
     </>
   )
